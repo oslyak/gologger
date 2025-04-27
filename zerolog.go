3fd56c125a62dbe88d985252bab5a6a823fb21b7
Anton Oslyak <oslyak@gmail.com>
2025-04-27 13:03:46 +0700

    The approach to obtaining the log directory and file name has been changed.

--- zerolog.go
+++ zerolog.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -19,7 +18,7 @@ func NewLogger(mode string) *zerolog.Logger {
 	case "prod", "production":
 		return NewProductionLogger()
 	case "test":
-		return NewTestLogger()
+		return NewTestLogger("log", "test.log")
 	default: // dev, development, or any other string
 		return NewDevelopmentLogger()
 	}
@@ -31,14 +30,21 @@ func NewProductionLogger() *zerolog.Logger {
 	return &logger
 }
 
-func NewTestLogger() *zerolog.Logger {
-	rootPath, err := GetProjectRoot()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, fmt.Errorf("error getting current working directory: %w", err).Error())
-		os.Exit(1)
+func NewTestLogger(logDir, filename string) *zerolog.Logger {
+	if filename == "" {
+		filename = "test.log"
 	}
 
-	logDir := filepath.Join(rootPath, "log")
+	if logDir == "" {
+		workingDir, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, fmt.Errorf("error getting working directory: %w", err).Error())
+			os.Exit(1)
+		}
+
+		logDir = filepath.Join(workingDir, "log")
+	}
+
 	if _, err := os.Stat(logDir); os.IsNotExist(err) {
 		err := os.Mkdir(logDir, os.ModePerm)
 		if err != nil {
@@ -47,7 +53,7 @@ func NewTestLogger() *zerolog.Logger {
 		}
 	}
 
-	logPath := filepath.Join(logDir, "test.log")
+	logPath := filepath.Join(logDir, filename)
 
 	const logFilePerm = 0o644
 	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, logFilePerm)
@@ -82,61 +88,4 @@ func NewDevelopmentLogger() *zerolog.Logger {
 	logger := zerolog.New(writer).With().Timestamp().Logger()
 
 	return &logger
-}
-
-// GetProjectRoot attempts to locate the project root by searching upward
-// from the current working directory for a marker file or directory.
-// In this example, we use "go.mod" and ".git" as markers.
-func GetProjectRoot() (string, error) {
-	// Start from the current working directory
-	currentDir, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-
-	for {
-		// Check if "go.mod" exists in the current directory
-		if exists, err := fileExists(filepath.Join(currentDir, "go.mod")); err == nil && exists {
-			return currentDir, nil
-		}
-
-		// Alternatively, check if the ".git" folder exists in the current directory
-		if exists, err := dirExists(filepath.Join(currentDir, ".git")); err == nil && exists {
-			return currentDir, nil
-		}
-
-		// Get the parent directory
-		parentDir := filepath.Dir(currentDir)
-		// If we've reached the root (parent is same as current), stop searching
-		if parentDir == currentDir {
-			break
-		}
-		currentDir = parentDir
-	}
-
-	return "", errors.New("project root not found: ensure that a marker file (e.g., go.mod or .git) exists in your project root")
-}
-
-// fileExists checks if the file at the given path exists and is not a directory.
-func fileExists(path string) (bool, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-		return false, err
-	}
-	return !info.IsDir(), nil
-}
-
-// dirExists checks if the directory at the given path exists.
-func dirExists(path string) (bool, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-		return false, err
-	}
-	return info.IsDir(), nil
 }
